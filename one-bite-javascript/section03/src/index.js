a23bdf5b651203bdd956932a421d9221909b1b2c
JunghunKum <123260423+kumgold@users.noreply.github.com>
2025-05-08 15:45:11 +0900

    section 03

--- one-bite-javascript/section03/src/index.js
+++ one-bite-javascript/section03/src/index.js
@@ -1,1 +1,6 @@
-console.log("Hi");
+import multiply, {add, subtract} from "./math.js";
+// const {add, sub} = require("./math");
+
+console.log(add(1, 2));
+console.log(subtract(2, 3));
+console.log(multiply(3, 4));
---
+++ one-bite-javascript/section03/src/math.js
@@ -0,0 +1,18 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export default function multiply(a, b) {
+    return a * b;
+}
+
+// export {add, subtract};
+
+// module.exports = {
+//     add: add,
+//     sub: subtract
+// }

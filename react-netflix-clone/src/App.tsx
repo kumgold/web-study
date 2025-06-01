33cbbb689294ad5eb6db1ca1a9edf6f24b837c14
JunghunKum <123260423+kumgold@users.noreply.github.com>
2025-06-01 23:20:21 +0900

    update App.css

--- react-netflix-clone/src/App.tsx
+++ react-netflix-clone/src/App.tsx
@@ -8,21 +8,19 @@ function App() {
         <div>
             <Header />
             <div>
-                <h1>영화, 시리즈 등을 무제한으로</h1>
-                <p>7000원으로 시작하세요. 멤버십은 언제든지 해지 가능합니다.</p>
+                <p>
+                    <span style={{ fontSize: 45, fontWeight: 800 }}>
+                        영화, 시리즈 등을 무제한으로
+                    </span>
+                    <br />
+                    7000원으로 시작하세요. 멤버십은 언제든지 해지 가능합니다.
+                </p>
                 <div>
                     <p>
                         시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면
                         이메일 주소를 입력하세요.
                     </p>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            flex: 1,
-                            alignItems: "stretch",
-                        }}
-                    >
+                    <div className="email_input">
                         <Input />
                         <Button text="시작하기" fontSize="18px" />
                     </div>

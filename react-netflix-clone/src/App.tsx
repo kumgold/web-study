dadb603022856510b39e90f53616df41c722492f
JunghunKum <123260423+kumgold@users.noreply.github.com>
2025-06-02 20:22:10 +0900

    refactor intro page header

--- react-netflix-clone/src/App.tsx
+++ react-netflix-clone/src/App.tsx
@@ -1,33 +1,11 @@
 import "./App.css";
-import Button from "./components/Button";
 import Header from "./components/Header";
-import Input from "./components/Input";
 
 function App() {
     return (
         <body>
             <Header />
             <main>
-                <section>
-                    <p>
-                        <span style={{ fontSize: 45, fontWeight: 800 }}>
-                            영화, 시리즈 등을 무제한으로
-                        </span>
-                        <br />
-                        7000원으로 시작하세요. 멤버십은 언제든지 해지
-                        가능합니다.
-                    </p>
-                    <div>
-                        <p>
-                            시청할 준비가 되셨나요? 멤버십을 등록하거나
-                            재시작하려면 이메일 주소를 입력하세요.
-                        </p>
-                        <div className="email_input">
-                            <Input />
-                            <Button text="시작하기" fontSize="18px" />
-                        </div>
-                    </div>
-                </section>
                 <section>
                     <div className="ad_membership">
                         <img src="../src/assets/popcorn.png" alt="" />
--- react-netflix-clone/src/components/Header.tsx
+++ react-netflix-clone/src/components/Header.tsx
@@ -1,27 +1,49 @@
 import Button from "./Button";
 import "./Header.css";
+import Input from "./Input";
 
-const Header: React.FC = () => {
+const Header = () => {
     return (
-        <main className="header">
-            <div className="header_left">
-                <img
-                    className="header_logo"
-                    src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-                    alt="Netflix Logo"
-                />
-            </div>
-            <div className="header_right">
-                <select className="header_select">
-                    <option lang="ko" label="한국어" value="ko-KR">
-                        한국어
-                    </option>
-                    <option lang="en" label="English" value="en-KR">
-                        English
-                    </option>
-                </select>
-                <Button text="로그인" fontSize="14px" />
-            </div>
+        <main className="container">
+            <section className="header">
+                <div className="header_left">
+                    <img
+                        className="header_logo"
+                        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                        alt="Netflix Logo"
+                    />
+                </div>
+                <div className="header_right">
+                    <select className="header_select">
+                        <option lang="ko" label="한국어" value="ko-KR">
+                            한국어
+                        </option>
+                        <option lang="en" label="English" value="en-KR">
+                            English
+                        </option>
+                    </select>
+                    <Button text="로그인" fontSize="14px" />
+                </div>
+            </section>
+            <section className="introduce">
+                <p>
+                    <span className="introduce_description">
+                        영화, 시리즈 등을 무제한으로
+                    </span>
+                    <br />
+                    7000원으로 시작하세요. 멤버십은 언제든지 해지 가능합니다.
+                </p>
+                <div>
+                    <p>
+                        시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면
+                        이메일 주소를 입력하세요.
+                    </p>
+                    <div className="email_input">
+                        <Input />
+                        <Button text="시작하기" fontSize="18px" />
+                    </div>
+                </div>
+            </section>
         </main>
     );
 };

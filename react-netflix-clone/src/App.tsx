d69c119dcf9e2cd303dd2d90c33459337cbf4de2
JunghunKum <123260423+kumgold@users.noreply.github.com>
2025-06-04 19:40:11 +0900

    Update App.tsx
    - delete body tag

--- react-netflix-clone/src/App.tsx
+++ react-netflix-clone/src/App.tsx
@@ -3,28 +3,26 @@ import Header from "./components/Header";
 
 function App() {
     return (
-        <body>
+        <main>
             <Header />
-            <main>
-                <section>
-                    <div className="ad_membership">
-                        <img src="../src/assets/popcorn.png" alt="" />
-                        <div className="ad_membership_container">
-                            <div>
-                                <span className="ad_membership_description">
-                                    7,000원이면 만날 수 있는 넷플릭스.
-                                </span>
-                                <br />
-                                <span>
-                                    가장 경제적인 광고형 멤버십을 이용해 보세요.
-                                </span>
-                            </div>
-                            <button>자세히 알아보기</button>
+            <section>
+                <div className="ad_membership">
+                    <img src="../src/assets/popcorn.png" alt="" />
+                    <div className="ad_membership_container">
+                        <div>
+                            <span className="ad_membership_description">
+                                7,000원이면 만날 수 있는 넷플릭스.
+                            </span>
+                            <br />
+                            <span>
+                                가장 경제적인 광고형 멤버십을 이용해 보세요.
+                            </span>
                         </div>
+                        <button>자세히 알아보기</button>
                     </div>
-                </section>
-            </main>
-        </body>
+                </div>
+            </section>
+        </main>
     );
 }
 

1987bb01c08f4709c2439851d45443779e190b15
JunghunKum <123260423+kumgold@users.noreply.github.com>
2025-06-16 16:28:02 +0900

    Netflix clone address

---
+++ react-netflix-clone/src/components/common/Address.tsx
@@ -0,0 +1,14 @@
+import "./Address.css";
+
+const Address = () => {
+    return (
+        <address>
+            <p>
+                질문이 있으신가요? 문의 전화:{" "}
+                <a href="tel:00-308-32100161">00-308-32100161 (수신자 부담)</a>
+            </p>
+        </address>
+    );
+};
+
+export default Address;
